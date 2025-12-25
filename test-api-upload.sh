27662a7efef811ee7337ae9b925a8a9dfa264551
Sercan <sercan@yazici-mail.nl>
2025-12-25 08:28:15 +0100

    old files

--- test-api-upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILE="C:\Users\Administrator\Downloads\Factuur EU Transport week 45_2025-75.pdf"
-curl -X POST http://localhost:3000/api/invoices/import-pdf \
-  -H "Authorization: Bearer test-token" \
-  -F "file=@$FILE" \
-  -s | jq .
